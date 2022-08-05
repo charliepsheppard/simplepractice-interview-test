683e1afa4f9879b66d8661b0bf89ceb8a1fac678
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-08-05 13:27:12 -0700

    Complete requirement 2

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,6 +1,12 @@
 class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
+    @appointments = Appointment.all
+    render json: @appointments
+
+    # doctor = Doctor.find(1)
+    # render json: doctor
+
     # TODO: return filtered values
     head :ok
   end
