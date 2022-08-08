80c2f699faf10c25ee3f051e9c309ce6e6675f64
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-08-08 07:51:05 -0700

    Add filter appointments

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,14 +1,17 @@
 class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
-    if params[:past] == 0
+    if params[:past] == "1"
       @appointments = Appointment.where(
-        
+        'start_time < ?', DateTime.now
       )
-    elsif
+    elsif params[:past] == "0"
+      @appointments = Appointment.where(
+        'start_time > ?', DateTime.now
+      )
     else
+      @appointments = Appointment.all
     end
-    @appointments = Appointment.all
     render json: @appointments
 
     # TODO: return filtered values
@@ -17,5 +20,12 @@ class Api::AppointmentsController < ApplicationController
 
   def create
     # TODO:
+
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:patient, :doctor, :start_time, :duration_in_minutes)
   end
 end
